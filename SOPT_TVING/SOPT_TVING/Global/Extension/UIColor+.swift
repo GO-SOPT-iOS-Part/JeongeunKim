be278a2c7c30c32dbded888681aef9e8c707b115
jeongdung-eo <jeuniis2@gmail.com>
2023-04-21 12:26:52 +0900

    [Add] #3 - 이미지,컬러 추가

--- SOPT_TVING/SOPT_TVING/Global/Extension/UIColor+.swift
+++ SOPT_TVING/SOPT_TVING/Global/Extension/UIColor+.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 extension UIColor {
+    
+    ///red
+
     static let tv_red = UIColor(named: "tv_red")
+    
+    /// gray
+    static let tv_gay1 = UIColor(named: "tv_gray/1")
+    static let tv_gay2 = UIColor(named: "tv_gray/2")
+    static let tv_gay3 = UIColor(named: "tv_gray/3")
+    static let tv_gay4 = UIColor(named: "tv_gray/4")
 
 }
