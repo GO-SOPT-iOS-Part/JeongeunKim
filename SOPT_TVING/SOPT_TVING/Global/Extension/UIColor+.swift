8287c89f65ee1a93fd9997b8373883096577c3c6
jeongdung-eo <jeuniis2@gmail.com>
2023-04-21 12:10:06 +0900

    [Add] #3 - Color 추가

---
+++ SOPT_TVING/SOPT_TVING/Global/Extension/UIColor+.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+.swift
+//  SOPT_TVING
+//
+//  Created by JEONGEUN KIM on 2023/04/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static let tv_red = UIColor(named: "tv_red")
+
+}
